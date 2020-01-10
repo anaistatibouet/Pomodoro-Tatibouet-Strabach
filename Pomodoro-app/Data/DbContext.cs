e5fd57fd6b5865d92bd80ee869cf53de2b215591
Unknown <benjamin.strabach@live.fr>
2020-01-10 15:16:41 +0100

    Création "Pomodoro" et "Session" en Codefirst
    /!\ chaîne de connexion non fonctionnelle

---
+++ Pomodoro-app/Data/DbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pomodoro.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+            : base("dbConnect")
+        {
+
+        }
+
+        public DbSet<Pomodoro> Pomodoros { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+
+    }
+}
---
+++ Pomodoro-app/Data/Pomodoro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pomodoro.Data
+{
+
+    [Table("pomodoro")]
+    public class Pomodoro
+    {
+        [Column("ID")]
+        [Key]
+        public int ID { get; set; }
+        [Column("Tag")]
+        [Required]
+        private int Tag { get; set; }
+        //Le tag d'un pomodoro sera une clée étrangère vers une table "tag"
+        public virtual Session Session { get; set; }
+    }
+}
