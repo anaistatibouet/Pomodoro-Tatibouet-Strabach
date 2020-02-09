bae9892dee823b2ccb30730cace21cd8ba54ddb6
Arkhun <darkana.polofowl@gmail.com>
2020-02-09 17:06:09 +0100

    Ajout formulaire session

---
+++ Pomodoro-app/Model/SessionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pomodoro.Model
+{
+    public class SessionModel
+    {
+        [Required]
+        [StringLength(20, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 16, ErrorMessage = "Choose a number between 0 and 16")]
+        public int NbPomodoros { get; set; }
+    }
+}
