6452bc92aa9e47e3d2511cb4f9e4790c09ea3c6c
Arkhun <darkana.polofowl@gmail.com>
2020-02-12 18:55:13 +0100

    Add tags form and link in session tab

---
+++ Pomodoro-app/Model/TagModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pomodoro.Model
+{
+    public class TagModel
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+    }
+}
